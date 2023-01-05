e58b78e4e2e241ec4f3fecebbbf2f7985b3201a7
Himan1999 <cu.16bcs1194@gmail.com[B>
2023-01-05 08:32:43 +0530

    expense app using full stack

---
+++ util/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', 'Himanshu@12',{
+    dialect:"mysql",
+    host:'localhost'
+})
+module.exports = sequelize;
